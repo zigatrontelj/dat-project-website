876127535a33a8c85e58920a93f989886b2dfac1
Žiga Trontelj <svizec96@gmail.com>
2019-01-21 18:12:13 +0000

    minor fixes

--- js/dat-project-website.js
+++ js/dat-project-website.js
@@ -30,8 +30,8 @@
   $(window).scroll(function (event) {
     var scroll = $(window).scrollTop();
     console.log(scroll);
-    $("#logod").css("top", 40-scroll/6+"%");
-    $("#logoa").css("top", 40-scroll/4+"%");
+    $("#logod").css("top", 40-scroll/4+"%");
+    $("#logoa").css("top", 40-scroll/6+"%");
     $("#logot").css("top", 40-scroll/3+"%");
 });
 
